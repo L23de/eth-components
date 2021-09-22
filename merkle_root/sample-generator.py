ab5f86ab30da8a7ba6c9f0d72e6dd84237a2af44
Lester Huang <huangl302d@gmail.com>
2021-09-21 21:25:45 -0400

    added to generate sample ledgers

---
+++ merkle_root/sample-generator.py
@@ -0,0 +1,16 @@
+import string
+import random
+
+STRING_LEN = 40
+
+def createSamples(file, lines: int):
+    for _ in range(lines):
+        hash = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(STRING_LEN))
+        file.write(hash)
+        file.write(" ")
+        file.write(str(random.randrange(0, 999)))
+        file.write("\0")
+
+file = open("ledger.txt", "w")
+createSamples(file, 20)
+
