28cf645578a48a138bf7d3325e865a3888c027be
Lester Huang <huangl302d@gmail.com>
2021-10-13 16:19:20 -0400

    update merkleRoot to get whole merkleTree

--- merkle_root/code/Hash.java
+++ merkle_root/code/Hash.java
@@ -1,6 +1,5 @@
 package merkle_root.code;
 
-import block.code.*;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
@@ -9,42 +8,49 @@ import java.util.ArrayList;
  * to a merge sort in terms of divide and conquer
  */
 public class Hash {
-    public static String getMerkleRoot(ArrayList<Account> ledger, int start, int end) {
-        // If recursion reaches a leaf, return an empty string
-        if (start == end) {
-
-            if (ledger.get(start).address == null) {
-                return "";
+    public static String[] getMerkleTree(ArrayList<Account> ledger, int size) {
+        String[] merkleTree = new String[2 * size + 1];
+        // Fill up the leaves
+        for (int i = 0; i <= size; i++) {
+            // Leaves are [size...2 * size]
+            // size is used as an offset
+            String hash = ledger.get(i).hash;
+            if (hash == null) { // In cases where there is a padded account
+                merkleTree[size + i] = "";
+            } else {
+                merkleTree[size + i] = hash;
             }
-            return ledger.get(start).hash;
         }
 
-        int mid = (start + end) / 2;
+        // Fill up the rest of the tree w/ hashes
+        // Condition is when merkleRoot is not found (At index 0)
+        for (int i = size - 1; i >= 0; i--) {
+            String leftHash = merkleTree[i * 2 + 1];
+            String rightHash = merkleTree[(i + 1) * 2];
+            String concatHash = leftHash + rightHash;
 
-        // Gets the left and right hashes of the tree
-        String leftHash = getMerkleRoot(ledger, start, mid);
-        String rightHash = getMerkleRoot(ledger, mid + 1, end);
-        String concatHash = leftHash + rightHash;
-
-        // If both left and right hashes are empty (Hash attempt of padded values),
-        // return nothing, since the hash of the empty string "" is still valid and it
-        // would modify the final resulting hash
-        if (concatHash == "") {
-            return "";
+            if (concatHash == "") {
+                // If both left and right hashes are empty (Hash attempt of padded values),
+                // return nothing, since the hash of the empty string "" is still valid and it
+                // would modify the final resulting hash
+                merkleTree[i] = "";
+            } else {
+                // Get the SHA-256 hash
+                try {
+                    merkleTree[i] = Encrypt.toHexString(Encrypt.getSHA(concatHash));
+                } catch (NoSuchAlgorithmException e) {
+                    System.err.println("ERROR: Unable to get SHA-256 hash");
+                    System.exit(1);
+                }
+            }
         }
 
-        // Get the SHA-256 hash of the leaf
-        try {
-            return Encrypt.toHexString(Encrypt.getSHA(concatHash));
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println("ERROR: Unable to get SHA-256 hash");
-            System.exit(1);
-        }
-        return "";
+        return merkleTree;
     }
 
-    public static ArrayList<String> getMembershipProof(ArrayList<Account> accList) {
-        ArrayList<String> proof;
-        return proof;
-    }
+    // public static ArrayList<String> getMembershipProof(ArrayList<Account>
+    // accList) {
+    // ArrayList<String> proof;
+    // return proof;
+    // }
 }
