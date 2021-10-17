e38b67d71baa43cb634ce2fca1a408921117c8d7
Lester Huang <huangl302d@gmail.com>
2021-10-17 18:28:00 -0400

    add comment

--- validation/code/App.java
+++ validation/code/App.java
@@ -1,12 +1,9 @@
 package validation.code;
 
-import block.code.*;
-import merkle_root.code.*;
-import validation.code.*;
-import java.util.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.security.NoSuchAlgorithmException;
+import java.util.LinkedList;
+import java.util.List;
+
+import block.code.Block;
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
@@ -21,6 +18,8 @@ public class App {
         List<Object> list = Validate.getBalance(blockchain, address);
 
         // Prints in a more neater (But still not very neat format)
+        // Unable to use the Gson package to pretty-print output, use
+        // http://jsonviewer.stack.hu/ to do so
         System.out.println("Found: " + list.get(0)); // Boolean
         System.out.println("\nProof of Membership:\n" + list.get(2)); // Proof (ArrayList<String>)
         System.out.println("\nHeaders:\n" + list.get(3)); // Headers (ArrayList<Hashtable<String, String>>)
